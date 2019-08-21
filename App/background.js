4045ba9eb45b56374c3d77cdbf08bd27b9f20e40
Martin Hartvig <martinhartvig@gmail.com>
2019-08-21 21:58:27 +0200

    Added shortcut

--- App/background.js
+++ App/background.js
@@ -6,7 +6,17 @@ var convert = "https://songwhip.com/convert?url="
 On page action click, redirect to songwhip
 */
 browser.pageAction.onClicked.addListener(async () => {
+ await redirect();
+});
+
+browser.commands.onCommand.addListener(async (command) => {
+    if (command == "shortcut-pressed") {
+      await redirect();
+    }
+});
+
+async function redirect(){
     var activeTab = await browser.tabs.query({active: true, currentWindow: true});
     var currentUrl = activeTab[0].url
-    chrome.tabs.update(activeTab[0].id, {url: convert+currentUrl, active:true});  
-});
+    chrome.tabs.update(activeTab[0].id, {url: convert+currentUrl, active:true}); 
+}
